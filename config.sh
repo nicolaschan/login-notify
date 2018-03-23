638c12cdca0e6b8a5585f20f9199e5a2e9a6a13a
Nicolas Chan <nicolas@nicolaschan.com>
2018-03-23 10:57:52 -0700

    Add config.sh

---
+++ config.sh
@@ -0,0 +1,5 @@
+# Replace these values with your own
+API_KEY="<Mailgun API Key>"
+MAILGUN_URL="https://api.mailgun.net/v3/<Mailgun Domain>.mailgun.org/messages"
+RECIPIENTS="Foo Bar <foo@example.com>, Baz Bar <baz@example.com>" # For multiple, comma separate
+FROM="SSH Alert <sshd@mycomputer.example.com>"
